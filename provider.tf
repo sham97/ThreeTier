204dac257cd325b85efa78a054d8d348f01bcba2
Illthizam9721 <illthizamn@kainovation.com>
2023-09-12 14:57:22 +0530

    remove unknown flags

--- provider.tf
+++ provider.tf
@@ -7,16 +7,18 @@ terraform {
   }
    backend "s3" {
     # Replace this with your bucket name!
-    # bucket         = "my-terraform-tfstate-three-tiers"
-    # key            = "terraform.tfstate"
-    # region         = "us-east-1"
+    bucket         = "my-terraform-tfstate-three-tiers"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
     # profile        = "sham"
+    # role_arn       = "arn:aws:iam::968017969736:role/my-terraform-three-tier-role"
    }
 }
 
-# Configure the AWS Provider
-# provider "aws" {
-#   region = "us-east-1"
-#   profile = "sham"
+#Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  # profile = "sham"
+  role_arn = "arn:aws:iam::968017969736:role/my-terraform-three-tier-role"
   
-# }
+}
